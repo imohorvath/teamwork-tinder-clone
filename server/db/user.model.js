3f5e326f9be7c6a52e43c1f62df14cc56399d4ba
Evelin Mandics <115423292+Maevenn@users.noreply.github.com>
2023-03-28 11:21:27 +0200

    edit user model

--- server/db/user.model.js
+++ server/db/user.model.js
@@ -4,11 +4,20 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
+  username: String,
   name: String,
   gender: String, 
   age: Number,
   hobbies: [String],
   introduction: String,
+  liked: [{
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  }],
+  rejected: [{
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  }],
   created: {
     type: Date,
     default: Date.now,
