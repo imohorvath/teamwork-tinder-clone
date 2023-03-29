1f00d4f250d1a1709295d0b55a3844b0418db17e
Noemi Varga <115186887+noemi-varga@users.noreply.github.com>
2023-03-29 18:33:48 +0200

    add profile menu elements

--- client/src/Pages/Profile/Profile.jsx
+++ client/src/Pages/Profile/Profile.jsx
@@ -1,34 +1,52 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./Profile.css";
 
 const Profile = () => {
   const { id } = useParams();
   const [user, setUser] = useState("");
+  const [showDetails, setShowDetails] = useState(false);
 
   useEffect(() => {
     fetch(`/api/users/${id}`)
       .then((res) => res.json())
       .then((user) => setUser(user));
-  }, []);
-
-  // useEffect(() => {
-  //   fetch(`https://api.pexels.com/v1/search/?page=3&per_page=80&query=male&size=medium`, {
-  //     headers: {
-  //       Authorization:
-  //         "563492ad6f917000010000016100bc354d8a41ee9d24ded961660c34",
-  //     },
-  //   })
-  //     .then((res) => res.json())
-  //     .then((data) => console.log(data));
-  // }, []);
+  }, [id]);
 
   return (
     <>
-    <div className="profile-container">
-      <h2>Hello {user.name}</h2>
-      <img src={user.image} alt="profile" />
-      </div>
+      {user && (
+        <div className="profile-container">
+          <h2>Hello {user.name.split(" ")[0]}!</h2>
+          <div className="profile-pic">
+            <img src={user.image} alt="profile" />
+          </div>
+          <div className="profile-menu">
+            <button
+              onClick={() => setShowDetails(true)}
+              className="profile-button button-personal-details"
+            >
+              Show my personal details
+            </button>
+            <Link>
+              <button className="profile-button button-matches">
+                My matches 🔥
+              </button>
+            </Link>
+            <Link>
+              <button className="profile-button button-favourites">
+                My picks 📌
+              </button>
+            </Link>
+            <Link>
+              <button className="profile-button button-second-chance">
+                Second chance 🔄
+              </button>
+            </Link>
+          </div>
+        </div>
+      )}
     </>
   );
 };
